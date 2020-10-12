db60d1ede847707eb67808d5c581d7f499759d1b
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-12 13:53:11 -0300

    Atribuições.

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 8
+let b = 3
+
+b += a // b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 8 // b = b * 8
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2 (if pair = 0, if odd = 1)
+console.log(b)
