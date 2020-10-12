31f77e2b406c85e15b97fc21137a1e9cb4c83f81
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-12 13:30:56 -0300

    Aula sobre par nome e valor

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,18 @@
+// par nome/valor
+const saudacao = 'Opa' //contexto léxico 1
+
+function exec() {
+    const saudacao = 'faalaa' //contexto léxico 2
+    return saudacao
+}
+
+//Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua braba',
+        numero: 123
+    }
+}
