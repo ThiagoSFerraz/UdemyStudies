ce91642c82832af342b7e25a9e06b96a3a8ab43e
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-12 15:25:19 -0300

    aula 2 destructuring

---
+++ fundamentos/destructuring2.js
@@ -0,0 +1,11 @@
+//WARNING -- AVISO!!! 
+//ISSO NÃO É ALGO QUE VOCÊ IRÁ USAR MUITO NO DIA A DIA!
+
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
+
+const [, [, nota]] = [[, 8, 8], [9, 6, 8]] 
+console.log(nota)
