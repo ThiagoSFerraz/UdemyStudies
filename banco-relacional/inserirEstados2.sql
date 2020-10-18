f2bc392d715c55d50e83ca7e1bf9d591b4db5853
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-18 10:21:47 -0300

    Inserindo mais estados

---
+++ banco-relacional/inserirEstados2.sql
@@ -0,0 +1,25 @@
+INSERT INTO estados
+    (nome, sigla, regiao, populacao)
+VALUES
+    ('Bahia', 'BA', 'Nordeste', 15.34),
+    ('Ceará', 'CE', 'Nordeste', 9.02),
+    ('Distrito Federal', 'DF', 'Centro-Oeste', 3.04),
+    ('Espírito Santo', 'ES', 'Sudeste', 4.02),
+    ('Goias', 'GO', 'Centro-Oeste', 9.02),
+    ('Maranhão', 'MA', 'Nordeste', 7.02),
+    ('Mato Grosso', 'MT', 'Centro-Oeste', 3.34),
+    ('Mato Grosso do Sul', 'MS', 'Centro-Oeste', 2.90),
+    ('Minas Gerais', 'MG', 'Sudeste', 21.62),
+    ('Pará', 'PA', 'Norte', 8.70),
+    ('Parana', 'PR', 'Sul', 11.72),
+    ('Pernambuco', 'PE', 'Nordeste', 9.52),
+    ('Piauí', 'PI', 'Nordeste', 9.02),
+    ('Rio de Janeiro', 'RJ', 'Sudeste', 16.98),
+    ('Rio Grande do Norte', 'RN', 'Nordeste', 3.72),
+    ('Rio Grande do Sul', 'RS', 'Sul', 11.02),
+    ('Rondônia', 'RO', 'Norte', 1.90),
+    ('Roraima', 'RR', 'Norte', 0.52),
+    ('Santa Catarina', 'SC', 'Sul', 7.02),
+    ('São Paulo', 'SP', 'Sudeste', 45.22),
+    ('Sergipe', 'SE', 'Nordeste', 2.29),
+    ('Tocantins', 'TO', 'Norte', 9.02);
