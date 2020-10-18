8fd5b95da3c239788a12c1f037b15a01ebd5ca1d
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-18 18:41:54 -0300

    Inserindo Cidades e usando ID dos estados

--- banco-relacional/inserirCidades.sql
+++ banco-relacional/inserirCidades.sql
@@ -9,3 +9,17 @@ VALUES (
     248.2,
     (SELECT id FROM estados WHERE sigla = 'CE')
 )
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES (
+    'Caruaru',
+    920.6,
+    (SELECT id FROM estados WHERE sigla = 'PE')
+)
+INSERT INTO cidades (nome, area, estado_id)
+VALUES (
+    'Niterói',
+    133.9,
+    (SELECT id FROM estados WHERE sigla = 'RJ')
+)
+DELETE FROM cidades WHERE id = 5
