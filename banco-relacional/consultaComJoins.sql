accaaa390dd04cde3cc1fcf89dceaa3a2cfdb4cf
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-18 21:26:22 -0300

    tipos de consulta com diferentes JOINS!

---
+++ banco-relacional/consultaComJoins.sql
@@ -0,0 +1,15 @@
+SELECT * FROM cidades;
+SELECT * FROM prefeitos;
+
+SELECT * FROM cidades c INNER JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c LEFT JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
+
+-- SELECT * FROM CIDADES C FULL JOIN  NÃO EXISTE, MAS PRA FAZER UMA "GAMBIARRA
+-- PODE SER FEITO :
+
+SELECT * FROM cidades c LEFT JOIN prefeitos p ON c.id = p.cidade_id
+UNION
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
