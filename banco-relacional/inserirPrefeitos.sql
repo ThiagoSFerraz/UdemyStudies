a258242c10039dedeb2d879bb75a582914366244
ThiagoSFerraz <oniferraz@gmail.com>
2020-10-18 21:02:21 -0300

    Inserindo Prefeitos 1.1

--- banco-relacional/inserirPrefeitos.sql
+++ banco-relacional/inserirPrefeitos.sql
@@ -1,3 +1,13 @@
 SELECT * FROM cidades;
 
-DELETE FROM prefeitos WHERE id= 8
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 6),
+    ('Raquel Lyra', 4),
+    ('Zenaldo Coutinho', null);
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Rafael Greca', null)
