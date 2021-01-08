a0efd3eed93bef6dce994cabe272e9bf98652976
ThiagoSFerraz <oniferraz@gmail.com>
2021-01-08 10:25:15 -0300

    no caso escrita KEKW

--- node/arquivoEscrita.js
+++ node/arquivoEscrita.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 const produto = {
     nome: 'Celular',
     preco: 1249.99,
-    desconto: 0.15
+    desconto: 0.15,
+    kek: "W"
 }
 
 fs.writeFile(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err =>{
