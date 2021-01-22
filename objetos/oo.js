f1208f85af7eaba021c9d06ed80845f532ecb76a
ThiagoSFerraz <oniferraz@gmail.com>
2021-01-22 15:37:17 -0300

    Visão teorica sobre orientação a objetos

---
+++ objetos/oo.js
@@ -0,0 +1,24 @@
+// Código não executável
+
+// Procedural
+
+Processamento(valor1, valor2, valor3)
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    Processamento() {
+        // ...
+    }
+}
+
+// Principios importantes em OO:
+
+// 1. Abstração
+
+// 2. Encapsulamento
+
+// 3. Herança
+
+// 4. Polimorfism
