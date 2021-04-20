27a8526243d4e66f35ecbb70f4e29a0297a217c6
ThiagoSFerraz <oniferraz@gmail.com>
2021-04-20 16:44:14 -0300

    espaçamento

--- projetoNode/src/db.js
+++ projetoNode/src/db.js
@@ -1,30 +1,30 @@
 const sequence = {
-  _id: 1,
-  get id() {
-    return this._id++;
-  },
+    _id: 1,
+    get id() {
+        return this._id++;
+    },
 };
 
 const produtos = {};
 
 function salvarProduto(produto) {
-  if (!produto.id) produto.id = sequence.id;
-  produtos[produto.id] = produto;
-  return produto;
+    if (!produto.id) produto.id = sequence.id;
+    produtos[produto.id] = produto;
+    return produto;
 }
 
 function getProduto(id) {
-  return produtos[id] || {};
+    return produtos[id] || {};
 }
 
 function getProdutos() {
-  return Object.values(produtos);
+    return Object.values(produtos);
 }
 
 function excluirProduto() {
-  const produto = produto[id];
-  delete produtos[id];
-  return produto;
+    const produto = produto[id];
+    delete produtos[id];
+    return produto;
 }
 
 module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto };
--- projetoNode/src/server.js
+++ projetoNode/src/server.js
@@ -9,35 +9,35 @@ const port = 3004;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/produtos", (req, res, next) => {
-  res.send(dataBase.getProdutos());
+    res.send(dataBase.getProdutos());
 });
 
 app.get("/produtos/:id", (req, res, next) => {
-  res.send(dataBase.getProduto(req.params.id));
+    res.send(dataBase.getProduto(req.params.id));
 });
 
 app.post("/produtos", (req, res, next) => {
-  const produto = dataBase.salvarProduto({
-    nome: req.body.nome,
-    preco: req.body.preco,
-  });
-  res.send(produto);
+    const produto = dataBase.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco,
+    });
+    res.send(produto);
 });
 
 app.put("/produtos/:id", (req, res, next) => {
-  const produto = dataBase.salvarProduto({
-    id: req.params.id,
-    nome: req.body.nome,
-    preco: req.body.preco,
-  });
-  res.send(produto);
+    const produto = dataBase.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco,
+    });
+    res.send(produto);
 });
 
 app.delete("/produtos/:id", (req, res, next) => {
-  const produto = dataBase.excluirProduto(req.params.id);
-  res.send(produto);
+    const produto = dataBase.excluirProduto(req.params.id);
+    res.send(produto);
 });
 
 app.listen(port, () => {
-  console.log(`The server is running in the ${port} port`);
+    console.log(`The server is running in the ${port} port`);
 });
